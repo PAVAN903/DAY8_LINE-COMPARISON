564f95f7b3f64c345a42d05a515bf419e0c94aa5
PAVAN903 <navap903@gmail.com>
2023-02-27 22:03:44 +0530

    added:equals lines length

--- src/com/bridgelab/LineComparison.java
+++ src/com/bridgelab/LineComparison.java
@@ -5,17 +5,26 @@ import java.util.Scanner;
 public class LineComparison {
 	public void line() {
 		double lengthOfline;
+		double lengthOfline1,lengthOfline2;
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter first line co ordinate x1");
-		int x1=sc.nextInt();
-		System.out.println("enter first line co ordinate x2");
-		int x2=sc.nextInt();
-		System.out.println("enter second line co ordinate y1");
-		int y1=sc.nextInt();
-		System.out.println("enter second line co ordinate");
-		int y2=sc.nextInt();
-		 lengthOfline=(Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) ));
-		System.out.println("length of line is "+lengthOfline);
+		System.out.println("enter line co ordinate x1,x2 and y1,y2");
+		 lengthOfline1=getLength(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt());
+		 System.out.println("enter line co ordinate x1,x2 and y1,y2");
+		 lengthOfline2=getLength(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt());
+		 System.out.println("value of line of length 1 ::"+lengthOfline1);
+		 System.out.println("value of line of length 2 ::"+lengthOfline2);
+		 if(lengthOfline1==lengthOfline2) {
+			 System.out.println("both lines are equal");
+		 }else {
+			 System.out.println("both lines are not equal"); 
+		 }
+		
+		
+	}
+
+	private double getLength(double x1,double x2,double y1, double y2) {
+		
+		return (( Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) ) ));
 	}
 
 	public static void main(String[] args) {
